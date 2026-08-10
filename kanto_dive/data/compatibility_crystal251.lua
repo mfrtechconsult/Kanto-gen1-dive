2c95e9e74f8bd85422e53d29c15520c3d01840c8
M <160327578+mfrtechconsult@users.noreply.github.com>
2026-08-10 07:42:07 +0200

    add Crystal 251 DIVE compatibility list

---
+++ kanto_dive/data/compatibility_crystal251.lua
@@ -0,0 +1,9 @@
+-- Generation II species that can learn HM08 DIVE in Pokemon Ruby/Sapphire/Emerald.
+-- Loaded only when Crystal 251 is active and its imported species are available.
+return {
+  "TOTODILE", "CROCONAW", "FERALIGATR",
+  "CHINCHOU", "LANTURN",
+  "MARILL", "AZUMARILL",
+  "POLITOED", "WOOPER", "QUAGSIRE", "SLOWKING", "QWILFISH",
+  "REMORAID", "OCTILLERY", "MANTINE", "KINGDRA", "SUICUNE", "LUGIA",
+}
